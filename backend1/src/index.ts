54ba69d5bb77bb4a9d930db721193adbf0e689ea
Safal Karki <sawfal24@gmail.com>
2025-07-05 14:13:28 +0000

    feat: updated page title and port listen message was more verbose

--- backend1/src/index.ts
+++ backend1/src/index.ts
@@ -36,5 +36,5 @@ app.use(errorLogMiddle);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(`Server is now running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
