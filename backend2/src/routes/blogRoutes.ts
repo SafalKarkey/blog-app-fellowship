eb42e94ad1b27b364317b11caa2f152b372daac7
Safal Karki <sawfal24@gmail.com>
2025-07-06 15:29:28 +0545

    feat: fix blog not created issue

--- backend2/src/routes/blogRoutes.ts
+++ backend2/src/routes/blogRoutes.ts
@@ -1,19 +1,12 @@
 import { Router } from 'express';
 import { createBlog, getAllBlogs, getBlog } from '../controllers/blogController';
 // Import your Clerk (or auth) middleware
-// import { ClerkExpressRequireAuth } from '@clerk/clerk-sdk-node';
+import { ClerkExpressRequireAuth } from '@clerk/clerk-sdk-node';
 
 const router = Router();
 
-// Add debugging middleware
-router.use((req, res, next) => {
-  console.log(`${req.method} ${req.path} - Headers:`, req.headers);
-  next();
-});
-
-// Protect only the createBlog route - temporarily disable auth for testing
-// router.post('/', ClerkExpressRequireAuth(), createBlog);
-router.post('/', createBlog);
+// Protect only the createBlog route
+router.post('/', ClerkExpressRequireAuth(), createBlog);
 router.get('/', getAllBlogs);
 router.get('/:id', getBlog);
 
